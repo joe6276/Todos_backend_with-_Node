697428a233d15583b31781cb18b95c39a4b167c1
Jonathan Ndambuki <69194082+joe6276@users.noreply.github.com>
2022-11-23 15:19:25 +0300

    Update dbhelper.js

--- DatabaseHelpers/dbhelper.js
+++ DatabaseHelpers/dbhelper.js
@@ -1,49 +1,53 @@
-const mssql = require('mssql')
- const sqlConfig= require('../Config/index')
-class Connection { 
+
+
+import sql from 'mssql'
+import sqlConfig from '../Config/config'
+
+
+export default class Connection{
+      private pool: Promise<sql.ConnectionPool>
     constructor(){
-        this.connecttoDb()
+       this.pool = this.connectToDb()
     }
-    // creating a connection pool
-    connecttoDb= async()=>{
+
+    connectToDb= async()=>{
         try {
-          this.pool = await mssql.connect(sqlConfig)  
-        } catch (error) {
+            const pool =await sql.connect(sqlConfig) 
+            return pool
+        } catch (error:any) {
+            console.log(error.message);
             throw new Error(error.message)
         }
     }
 
-    createRequest=async(request, data={})=>{
-        const keys = Object.keys(data)
-        keys.map((keyName)=>{
-         const keyValue=data[keyName]   
-         request.input(keyName, keyValue)
+    createRequest = (request:sql.Request, data:{[c:string]:string|number|boolean})=>{
+     try {
+           const keys = Object.keys(data)
+
+        keys.map((keyname:string)=>{
+            const keyValue= data[keyname]
+            request.input(keyname, keyValue)
         })
-        return request
+
+        return request;
+     } catch (error:any) {
+       return error.message
+     }
     }
 
-    exec=async(storedproc, data={})=>{
-      try {
-        let request= await this.pool.request()  
-      request= await this.createRequest(request,data)
-      const result = await request.execute(storedproc)
-      return result
-      } catch (error) {
-        throw new Error(error.message)
-      }
-    }
-    query=async(query)=>{
-       try {
-         const result= await this.pool.request().query(query)
-        return result
-       } catch (error) {
-        throw new Error(error.message)
-       }
+    exec= async(procedure:string, data={})=>{
+   try {
+    let request = await (await this.pool).request() as sql.Request ;
+    request= this.createRequest(request, data)
+    const results =await request.execute(procedure)
+    return results
+   } catch (error:any) {
+    throw new Error(error.message)
+   }
     }
 
-}
-
-module.exports={
-    exec: new Connection().exec,
-    query: new Connection().query
+    query= async(query:string)=>{
+        const results = await (await this.pool).request().query(query)
+        return results
+    }
 }
